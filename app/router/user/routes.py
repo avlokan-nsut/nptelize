ef9d390480b184ff57df6579a6fb8aefb4851090
shrey <shrey@kali>
2025-05-14 22:22:33 +0530

    update: login response schema

--- app/router/user/routes.py
+++ app/router/user/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from app.config.db import get_db
 from app.models import User, UserRole
-from app.router.user.schemas import LoginRequest
+from app.router.user.schemas import LoginRequest, LoginResponse
 from app.services.utils.hashing import verify_password_hash
 from app.oauth2 import create_access_token
 from typing import cast
@@ -10,7 +10,7 @@ from typing import cast
 
 router = APIRouter(prefix='/user')
 
-@router.post("/login")
+@router.post("/login", response_model=LoginResponse)
 def login(
     role: UserRole, credentials: LoginRequest, response: Response, db: Session = Depends(get_db),
 ):
@@ -39,4 +39,9 @@ def login(
         samesite="None",
     )
 
-    return {'message': "Login successful", 'user_id': user.id}
+    return {
+            'message': "Login successful", 
+            'user_id': user.id,
+            'name': user.name
+        }
+    
--- app/router/user/schemas.py
+++ app/router/user/schemas.py
@@ -1,6 +1,20 @@
 from pydantic import BaseModel
 
 
+# -----------------------------------------------------------------------
+# Request Schemas
+# -----------------------------------------------------------------------
+
 class LoginRequest(BaseModel):
     email: str
     password: str
+
+
+# -----------------------------------------------------------------------
+# Response Schemas
+# -----------------------------------------------------------------------
+
+class LoginResponse(BaseModel):
+    message: str
+    user_id: str
+    name: str
